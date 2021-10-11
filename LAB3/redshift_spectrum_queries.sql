fa4028f6bd75a53711de239b645aeae11aefa5fe
Veena Vasudevan <vasveena@amazon.com>
2021-10-11 13:38:33 -0400

    changes

--- LAB3/redshift_spectrum_queries.sql
+++ LAB3/redshift_spectrum_queries.sql
@@ -2,46 +2,30 @@ drop schema if exists spectrum;
 
 create external schema spectrum
 from data catalog
-database 'testdb'
+database 'default'
 region 'us-east-1' 
-iam_role 'arn:aws:iam::620614497509:role/DemoSpectrumRole';
+iam_role 'arn:aws:iam::166573744155:role/DemoSpectrumRole';
 
-drop table if exists spectrum.trip;
-
-//Not required for Hudi table 
-
-create external table spectrum.trip(
-tripId varchar(20),
-routeId varchar(20),
-startTime timestamp,
-arrivalTime array<varchar(20)>,
-departureTime array<varchar(20)>,
-futureStopIds array<varchar(20)>,
-numOfFutureStops integer,
-currentStopSequence integer,
-currentStatus varchar(20),
-stopId varchar(20),
-currentTs varchar(20)
-)
-stored as parquet
-location 's3://your_s3_bucket/prefix/streaming_output/';
+select tripId, routeId, startTime, numOfFutureStops, 
+currentStopSequence, currentStatus, stopId, currentTs
+from spectrum.unknown limit 10;
 
 --select all non-array fields 
 select tripId, routeId, startTime, numOfFutureStops, 
 currentStopSequence, currentStatus, stopId, currentTs
-from spectrum.trip limit 10;
+from spectrum.unknown limit 10;
 
 --select future stop IDs for a trip  
-select distinct f from spectrum.trip t 
+select distinct f from spectrum.unknown t 
 join t.futureStopIds f on true 
-where tripId = '019250_E..S'
+where tripId = '081100_FS..N'
 order by currentTs;
 
 --Total number of entries
-select count (tripId) from spectrum.trip 
+select count (tripId) from spectrum.unknown
 
 -- Number of trips started between two intervals 
-select count(*) as numOfTrips, tripId, routeId, startTime from spectrum.trip 
+select count(*) as numOfTrips, tripId, routeId, startTime from spectrum.unknown 
 where startTime BETWEEN '2021-09-20 02:30:00' AND '2021-09-20 03:05:00'
 group by tripId, routeId, startTime order by 4 desc;
 
@@ -49,7 +33,7 @@ group by tripId, routeId, startTime order by 4 desc;
 select tripId, routeId, currentTs, sum(numOfFutureStops) from
 (select tripId , routeId, currentTs, numOfFutureStops,
 rank() over (order by currentTs) as rnk
-from spectrum.trip)
+from spectrum.unknown)
 where rnk = 1
 group by 1,2,3
 order by 4 desc;
